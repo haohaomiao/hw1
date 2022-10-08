e4a6a868eb1e88ef25dcc5013bd09b907e59bd3a
haohaomiao <haohaomiao@outlook.com>
2022-10-08 21:06:13 +0800

    version1.0: modified lint.sh 完成格式化

--- lint.sh
+++ lint.sh
@@ -1,11 +1,11 @@
 # TODO: add autopep8 here.
-
+autopep8 --in-place -r -a  ./
 
 # TODO: add autoflake here.
-
+autoflake --in-place -r --remove-unused-variables ./
 
 # TODO: add isort here.
-
+isort ./
 
 # TODO: add flake8 here.
 flake8 .
