00bd22a811f38b1322413f490103b5b801723795
haohaomiao <haohaomiao@outlook.com>
2022-10-10 14:13:16 +0800

    version2.0:完成了单元测试 修改user.py, test_basic.py 覆盖率92%

--- app/checkers/user.py
+++ app/checkers/user.py
@@ -1,7 +1,49 @@
 # -*- coding: utf-8 -*-
-
+# from nis import match
+import re
 def register_params_check(content):
     """
     TODO: 进行参数检查
     """
+    # 检查username
+    username = content.get('username')
+    if username is None:
+        return "username",False
+    elif (re.search('^[A-Za-z]+[0-9]+$',username) is None) or (re.search('^.{5,12}$',username) is None):
+        return "username",False
+    # 检查password
+    password = content.get('password')
+    if password is None:
+        return "password",False
+    elif((re.search('^[A-Za-z0-9\-\_\*\.]{8,15}$',password) is None)  or 
+        (re.search('[A-Z]',password) is None) or 
+        (re.search('[a-z]',password) is None) or 
+        (re.search('[0-9]',password) is None) or
+        (re.search('[\-\_\*\.]',password) is None)
+        ):
+        return "password",False
+    # 检查nickname
+    nickname = content.get('nickname')
+    if nickname is None or nickname == '':
+        return "nickname",False
+    # 检查url
+    url = content.get('url')
+    if url is None:
+        return "url",False
+    elif((re.search('https?://([A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]\.)+[0-9A-Za-z]+',url) is None) or 
+        (re.search('\.[^0-9]+$',url) is None) or
+        (re.search('^.{,48}$',url) is None)):
+        return "url",False
+    # 检查mobile
+    mobile = content.get('mobile')
+    if mobile is None:
+        return "mobile",False
+    elif((re.search('^\+[0-9]{2}\.[0-9]{12}$',mobile) is None)):
+        return "mobile",False
+    # 检查magic_number
+    magic_number = content.get('magic_number')
+    if magic_number is None or magic_number == "":
+        content['magic_number'] = 0
+    elif re.search('^[0-9]+$',magic_number) is None:
+        return "magic_number",False
     return "ok", True
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+from cgi import test
 import unittest
 
 from app.checkers.user import register_params_check
@@ -8,7 +9,221 @@ class BasicTestCase(unittest.TestCase):
     TODO: 在这里补充注册相关测试用例
     '''
     def test_register_params_check(self):
-        self.assertEqual(register_params_check(None), ("ok", True))
+    # 正常测例 
+        # 1.username和password长度为下边界done
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("ok",True))
+        # 2.username和password长度为上边界done
+        self.assertEqual(register_params_check({'username':'testtesttes1',
+                                                'password':'Test1_test12345',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("ok",True))
+    # username异常测例
+        # 1.缺少username字段done
+        self.assertEqual(register_params_check({
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+        # 2.lenth < 5done
+        self.assertEqual(register_params_check({'username':'test',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+        # 3.len > 12done
+        self.assertEqual(register_params_check({'username':'test1test1123',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+        # 4.缺少字母done
+        self.assertEqual(register_params_check({'username':'12345',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+        # 5.缺少数字done
+        self.assertEqual(register_params_check({'username':'testa',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+        # 6.字母在数字后面
+        self.assertEqual(register_params_check({'username':'1test',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("username",False))
+    # password异常测例
+        # 1.缺少password
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 2.len<8
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_1',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 3.len>15
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_1290123456',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 4.缺少A
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 5.缺少a
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'TEST1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 6.缺少数字
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Testa_bc',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 7.缺少标点
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1a12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+        # 8.出现其它标点
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1%12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("password",False))
+    # nickname异常测例
+        # 1.缺失nickname
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("nickname",False))
+    # url异常测例
+        # 1.url缺失
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        # 2.协议错误
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'htps://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https123://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        # 3.域名 长度过长
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com.baidu.com.baidu.com.baiduaa.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        # 4.域名 最后一段纯数字
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.123',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+        # 5.域名 连字符为首尾字符
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.-com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':'114514'}),("url",False))
+# 手机号异常测例
+        # 1.缺少手机号
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'magic_number':'114514'}),("mobile",False))
+        # 2.区号异常测例
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'86.123456789012',
+                                                'magic_number':'114514'}),("mobile",False))
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+786.123456789012',
+                                                'magic_number':'114514'}),("mobile",False))
+        # 3.手机号异常测例
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.12345678901',
+                                                'magic_number':'114514'}),("mobile",False))
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.1234567890123',
+                                                'magic_number':'114514'}),("mobile",False))
+    # 幸运数字测例
+        # 1.幸运数字缺失
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012',
+                                                'magic_number':''}),("ok",True))
+        self.assertEqual(register_params_check({'username':'test1',
+                                                'password':'Test1_12',
+                                                'nickname':'nickname',
+                                                'url':'https://baidu.com',
+                                                'mobile':'+86.123456789012'}),("ok",True))
+        
 
 
 if __name__ == '__main__':
